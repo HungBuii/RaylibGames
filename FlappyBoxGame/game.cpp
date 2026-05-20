edb4863a229c17c2bb016da537ab1ea0fab2652f
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2026-05-20 22:37:25 +0700

    update: logic reveals water pipes
    - fix: errors in variable and struct naming conventions,...
    - delete: unnecessary comments

--- FlappyBoxGame/game.cpp
+++ FlappyBoxGame/game.cpp
@@ -1,53 +1,64 @@
 #include "iostream"
 #include "raylib.h"
 
+// Define value
+#define MAX_PIPES 6 // Count Water Pipe <= Count Water Pipe / 2 && Count Water Pipe >= 2
+#define WPWidth 80
+#define WPHeight 200
+#define RADIUS 30
+
 // Round Sphere Object (Main character - Flappy)
-typedef struct RSphere
+struct Flappy
 {
     Vector2 position;
     int radius;
     Color color;
-} Flappy;
+};
 
 // Obstacle Object (Water Pipe)
-typedef struct Obstacle
+typedef struct WaterPipe
 {
     Rectangle rec;
     Color color;
-} WaterPipe;
+} WP;
 
 // Global Variables Declaration
 static const int screenWidth = 800;
 static const int screenHeight = 450;
 
 static Flappy flappy;
-static WaterPipe waterpipe;
+static WP wp[MAX_PIPES] = {0};
 
 // Module Functions Declaration (local)
 static void InitGame(); // Initialize game
 static void DrawGame(); // Draw game (one frame)
 
-// Count Water Pipe <= Count Water Pipe / 2 && Count Water Pipe >= 2
-const int MAX = 6;
-
-// Properties of "water pipes"
-// int posX = 0;
-// int posY[MAX] = {};
-// int width = 80;
-// int height = 200;
-// Color color = GREEN;
+static Vector2 wpPos[MAX_PIPES] = {0};
 
 void InitGame()
 {
-    flappy.radius = 30;
+    flappy.radius = RADIUS;
     flappy.color = BLUE;
     flappy.position = (Vector2){80, screenHeight / 2};
 
-    waterpipe.rec.x = 400;
-    waterpipe.rec.y = 0;
-    waterpipe.rec.width = 80;
-    waterpipe.rec.height = 200;
-    waterpipe.color = GREEN;
+    for (int i = 0; i < MAX_PIPES; i++)
+    {
+        wpPos[i].x = 400 + 250 * i;
+        wpPos[i].y = GetRandomValue(-50, 0);
+    }
+
+    for (int i = 0; i < MAX_PIPES; i += 2)
+    {
+        wp[i].rec.x = wpPos[i / 2].x;
+        wp[i].rec.y = wpPos[i / 2].y;
+        wp[i].rec.width = WPWidth;
+        wp[i].rec.height = WPHeight;
+
+        wp[i + 1].rec.x = wpPos[i / 2].x;
+        wp[i + 1].rec.y = wp[i].rec.height + wpPos[i / 2].y + RADIUS * 2 + 40;
+        wp[i + 1].rec.width = WPWidth;
+        wp[i + 1].rec.height = WPHeight;
+    }
 }
 
 void DrawGame()
@@ -58,20 +69,16 @@ void DrawGame()
     // Round Sphere (Main character)
     DrawCircle(flappy.position.x, flappy.position.y, flappy.radius, flappy.color);
 
-    // Rectangle Water Pipe
-    DrawRectangle(waterpipe.rec.x, waterpipe.rec.y, waterpipe.rec.width, waterpipe.rec.height, waterpipe.color);
-    DrawRectangle(waterpipe.rec.x, waterpipe.rec.height + waterpipe.rec.y + flappy.radius * 2 + 40, waterpipe.rec.width, waterpipe.rec.height, waterpipe.color);
+    for (int i = 0; i < MAX_PIPES; i += 2)
+    {
+        DrawRectangle(wp[i].rec.x, wp[i].rec.y, wp[i].rec.width, wp[i].rec.height, GREEN);
+        DrawRectangle(wp[i + 1].rec.x, wp[i + 1].rec.y, wp[i + 1].rec.width, wp[i + 1].rec.height, GREEN);
+    }
 }
 
 int main()
 {
     InitWindow(screenWidth, screenHeight, "classic game: floppy");
-
-    // posY Rectangle Water Pipe
-    // for (int i = 0; i < MAX; i += 2)
-    // {
-    //     posY[i] = GetRandomValue(-50, 0);
-    // }
 
     InitGame();
 
@@ -80,30 +87,6 @@ int main()
         BeginDrawing();
 
         DrawGame();
-
-        // Rectangle Water Pipe
-        // DrawRectangle(400, 0, 80, 200, GREEN);
-        // DrawRectangle(400, 200 + 60 + 40, 80, 200, GREEN);
-
-        // DrawRectangle(400 + 250, -20, 80, 200, GREEN);
-        // DrawRectangle(400 + 250, 200 - 20 + 60 + 40, 80, 200, GREEN);
-
-        // int radiusCircle = 30;
-        // int posX = 0;
-        // int width = 80;
-        // int height = 200;
-        // Color color = GREEN;
-
-        // posX = 0;
-
-        // for (int i = 0; i < MAX; i += 2)
-        // {
-        //     DrawRectangle(posX, posY[i], width, height, color);
-        //     DrawRectangle(posX, height + posY[i] + radius * 2 + 40, width, height, color);
-
-        //     posX += 250;
-        //     std::cout << "posX = " << posX << "\n";
-        // }
 
         EndDrawing();
     }
