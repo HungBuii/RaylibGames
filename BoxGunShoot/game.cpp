aaad61176c33bd005609082f7143ed8686e7d3dc
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2026-05-28 14:35:14 +0700

    add: "main ship" character
    - add shape
    - add movement

--- BoxGunShoot/game.cpp
+++ BoxGunShoot/game.cpp
@@ -1,9 +1,19 @@
 #include "iostream"
 #include "raylib.h"
 
+// Spaceship (main character)
+struct Spaceship
+{
+    Rectangle rec;
+    Vector2 speed;
+    Color color;
+};
+
 // Global Variables Declaration
 static const int screenWidth = 800;
 static const int screenHeight = 450;
+
+static Spaceship spaceship;
 
 // Module Functions Declaration
 static void ShowFPS();         // Show FPS on screen
@@ -12,9 +22,33 @@ static void DrawGame();        // Draw game (one frame)
 static void UpdateGame();      // Update game (one frame)
 static void UpdateDrawFrame(); // Update and Draw
 
+// Logic Functions Declaration
+static void MoveButton();
+
+// Logic Functions Init
+void MoveButton()
+{
+    if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))
+        spaceship.rec.y -= spaceship.speed.y;
+    if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))
+        spaceship.rec.y += spaceship.speed.y;
+    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))
+        spaceship.rec.x -= spaceship.speed.x;
+    if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))
+        spaceship.rec.x += spaceship.speed.x;
+}
+
 // Module Functions Init
 void InitGame()
 {
+    // Init spaceship object
+    spaceship.rec.x = 20;
+    spaceship.rec.y = 50;
+    spaceship.rec.width = 40;
+    spaceship.rec.height = 20;
+    spaceship.speed.x = 5;
+    spaceship.speed.y = 5;
+    spaceship.color = BLUE;
 }
 
 void DrawGame()
@@ -24,11 +58,20 @@ void DrawGame()
     // Background
     ClearBackground(WHITE);
 
+    // Draw objects
+    DrawRectangle(spaceship.rec.x, spaceship.rec.y, spaceship.rec.width, spaceship.rec.height, spaceship.color); // Main ship
+
+    // DrawRectangle(50, 50, 40, 20, BLUE);  // Main ship
+    // DrawRectangle(100, 50, 20, 10, BLUE); // Bullet
+    // DrawRectangle(50, 100, 20, 20, BLUE); // Enemy
+
     EndDrawing();
 }
 
 void UpdateGame()
 {
+    // Spaceship movement
+    MoveButton();
 }
 
 void UpdateDrawFrame()
@@ -46,6 +89,8 @@ void ShowFPS()
 int main()
 {
     InitWindow(screenWidth, screenHeight, "classic game: box gun shoot");
+
+    InitGame();
 
     SetTargetFPS(60);
 
