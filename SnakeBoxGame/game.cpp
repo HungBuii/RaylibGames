c9683de8b8cc0e5a05b546b969c1d0022f486849
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2026-06-09 21:26:10 +0700

    add: "snake"
    - add: "platform" for "snake" move on
    - create "snake" character

--- SnakeBoxGame/game.cpp
+++ SnakeBoxGame/game.cpp
@@ -1,24 +1,74 @@
 #include "iostream"
 #include "raylib.h"
 
+#define SIZE_SQUARE 31
+// #define SNAKE_LENGTH 350
+
+// Snake
+struct Snake
+{
+    Vector2 position;
+    Vector2 size;
+    Vector2 speed;
+    Color color;
+};
+
 // Global Variables Declaration
 static const int screenWidth = 800;
 static const int screenHeight = 450;
 
+static Vector2 offset;
+
+static Snake snake;
+static int snake_length = 0;
+
 // Module Functions Declaration (local)
-// static void InitGame();        // Initialize game
+static void InitGame(); // Initialize game
 static void DrawGame(); // Draw game (one frame)
 // static void UpdateGame();      // Update game (one frame)
 static void UpdateDrawFrame(); // Update and Draw
 static void ShowFPS();         // Show FPS on screen        // Show FPS on screen
 
 // Module Functions Init (local)
+void InitGame()
+{
+    // x, y coordinates according to screen resolution
+    offset.x = screenWidth % SIZE_SQUARE;
+    offset.y = screenHeight % SIZE_SQUARE;
+
+    // init "snake"
+    snake_length = 1;
+    snake.position = (Vector2){offset.x / 2, offset.y / 2};
+    snake.size = {SIZE_SQUARE, SIZE_SQUARE};
+}
+
 void DrawGame()
 {
     BeginDrawing();
 
     // Background
     ClearBackground(WHITE);
+
+    // Horizontal lines "Surface"
+    for (int i = 0; i <= screenHeight / SIZE_SQUARE; i++)
+    {
+        DrawLineV(
+            (Vector2){offset.x / 2, (offset.y / 2) + (SIZE_SQUARE * i)},
+            (Vector2){screenWidth - (offset.x / 2), (offset.y / 2) + (SIZE_SQUARE * i)},
+            GRAY);
+    }
+
+    // Vertical lines "Surface"
+    for (int i = 0; i <= screenWidth / SIZE_SQUARE; i++)
+    {
+        DrawLineV(
+            (Vector2){(offset.x / 2) + (SIZE_SQUARE * i), offset.y / 2},
+            (Vector2){(offset.x / 2) + (SIZE_SQUARE * i), screenHeight - (offset.y / 2)},
+            GRAY);
+    }
+
+    // Snake
+    DrawRectangleV(snake.position, snake.size, DARKBLUE);
 
     EndDrawing();
 }
@@ -40,9 +90,11 @@ int main()
 
     SetTargetFPS(60);
 
+    InitGame();
+
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
-        ShowFPS();
+        // ShowFPS();
 
         UpdateDrawFrame();
     }
